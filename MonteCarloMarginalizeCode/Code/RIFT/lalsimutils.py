f624e4c7ec5207749500321f11733705a9b27b80
Aasim Z Jan <58118417+AasimZahoor@users.noreply.github.com>
2024-10-31 19:35:34 -0500

    lalsimutils.py: fixed a comment

--- MonteCarloMarginalizeCode/Code/RIFT/lalsimutils.py
+++ MonteCarloMarginalizeCode/Code/RIFT/lalsimutils.py
@@ -4743,7 +4743,7 @@ def hlmoff_for_LISA(P, Lmax=4, modes=None, fd_standoff_factor=0.964, fd_alignmen
         # convert into dictionary
         hlmsdict = SphHarmFrequencySeries_to_dict(hlms_struct, Lmax, modes)
         # Resize it such that deltaF = 1/TDlen
-	# Commenting this out because this chages fmax without changing P.fmax; tf_dict doesn't use fmax any more but this function add zeros to the end, but evaluate fvals expects the data to be centered around zero.
+	    # Commenting this out because this chages fmax without changing P.fmax; tf_dict doesn't use fmax any more but this function add zeros to the end, but evaluate fvals expects the data to be centered around zero.
         for mode in hlmsdict:
             hlmsdict[mode] = lal.ResizeCOMPLEX16FrequencySeries(hlmsdict[mode],0, TDlen)
         return hlmsdict
