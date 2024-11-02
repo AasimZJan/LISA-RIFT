4ba62108a0f35135c4c6be3bbd6983232f59e52a
AasimZahoor <zahoorassim@gmail.com>
2024-11-02 12:48:33 -0700

    generate_injections.py: added tapering option

--- MonteCarloMarginalizeCode/Code/RIFT/LISA/injections/generate_injections.py
+++ MonteCarloMarginalizeCode/Code/RIFT/LISA/injections/generate_injections.py
@@ -1,29 +1,31 @@
 from RIFT.LISA.injections.LISA_injections import *
-
+import os
+import numpy as np
 param_dict = {}
 
 
-param_dict.update({'m1':1757638.40382,
-                   'm2':1300396.8986,
-                   's1z':0.50977418,
-                   's2z':0.13947856,
-                   'beta':0.29339096,
-                   'lambda':2.27853319,
-                   'dist':33703.05400678,
-                   'inclination':1.83510232,
-                   'phi_ref':4.22856809,
-                   'psi':1.24272195,
-                   'tref':6712404.50294161,
+param_dict.update({'m1':15e6,
+                   'm2':15e6,
+                   's1z':0.0,
+                   's2z':0.0,
+                   'beta':np.pi/3,
+                   'lambda':np.pi/4,
+                   'dist':274.566 * 1e3,
+                   'inclination':0.0,
+                   'phi_ref':5*np.pi/4,
+                   'psi':np.pi/6,
+                   'tref':2621440.0*2,
                    'deltaT':5,
-                   'deltaF':float(1.0/(41943040)),
+                   'deltaF':float(1.0/(2621440.0*4)),
                    'fmin':0.00008,
-                   'fref':None,
+                   'fref':0.0005510330200195313,
                    'wf-fref':0.00008,
                    'approx':"IMRPhenomD",
-                   'modes':[(2,2)],
-                   'save_path':"/Users/aasim/Desktop/Research/Projects/RIFT_LISA/Development/Testing/bias_in_sangria/frames_new",
-                   'path_to_NR_hdf5':None, 
-                   'psd_path':"/Users/aasim/Desktop/Research/Projects/RIFT_LISA/Development/Testing/bias_in_sangria/psds",
+                   'modes':[(2,2),(2,1),(3,3),(3,2),(3,1),(4,4),(4,3),(4,2)],
+                   'save_path':f"{os.getcwd()}/frames",
+                   'path_to_NR_hdf5':"/home/aasim.jan/mbhb_accuracy/SXS/h5_files/SXS_BBH_0002_Res4.h5",
+                   'NR_taper_percent':2.5,
+                   'psd_path':f"{os.getcwd()}",
                    'snr_fmin':0.0001,
                    'snr_fmax':0.1})
 
